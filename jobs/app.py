0460804039bc0fb5aaf3e98573806d282fd03293
Steven Yu <Stevenyu@mail.weber.edu>
2020-10-28 15:56:27 -0600

    'commit'

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+
+PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
+def open_connection():
+    getattr('_connection', g, None)
+    return getattr()
 
 @app.route('/')
 @app.route('/jobs')
