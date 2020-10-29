d9a450f037b97e48c963d966ef576e3b25c7364b
Steven Yu <Stevenyu@mail.weber.edu>
2020-10-29 12:58:15 -0600

    'commit'

--- jobs/app.py
+++ jobs/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, g
 import sqlite3
+from flask import Flask, render_template, g
 
 PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
+
 def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
@@ -31,7 +32,5 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, '
-                       'employer.id as employer_id, employer.name as employer_name '
-                       'FROM job JOIN employer ON employer.id = job.employer_id')
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
     return render_template('index.html', jobs=jobs)
